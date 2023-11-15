4b71cb8e17774a2e0c22dac225288f722af70662
Roman Khussein <romanfarri@gmail.com>
2023-11-15 10:59:04 +0200

    івдлаоівда

---
+++ Lab_6_MTZKIT/file.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace lab {
+	class Solution
+	{
+		static void Main(string[] args)
+		{
+			int x = 5;
+			int y = 10;
+			Console.WriteLine("5 + 10 = {0}", x + y);
+			Console.ReadKey();
+		}
+	}	
+}
