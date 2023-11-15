c081fcc5edbc54046b21f3dc0dffb72c4d0fbfb0
Roman Khussein <romanfarri@gmail.com>
2023-11-15 10:14:17 +0200

    Додано підрахунок суми чисел і виведення результату

--- Lab_6_MTZKIT/Program.cs
+++ Lab_6_MTZKIT/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace lab {
+	class Solution
+	{
+		static void Main(string[] args)
+		{
+			int x = 5;
+			int y = 10;
+			Console.WriteLine("5 + 10 = {0}", x + y);
+			Console.ReadKey();
+		}
+	}	
+}
